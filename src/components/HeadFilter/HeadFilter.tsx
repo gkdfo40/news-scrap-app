657d22dbc4fc85b182548e02b741b020e6d0e7bc
gkdfo40 <gkdfo40@gmail.com>
2022-07-04 12:56:06 +0900

    feat: add Header Filter style

--- src/components/HeadFilter/HeadFilter.tsx
+++ src/components/HeadFilter/HeadFilter.tsx
@@ -1,10 +1,14 @@
 import { MagnifierIcon, CalenderIcon } from 'assets/svgs'
 
-import { useAppDispatch } from 'hooks/reduxhook'
+import { useAppDispatch, useAppSelector } from 'hooks/reduxhook'
 import { openModal } from 'store/modalSlice'
 import styles from './headFilter.module.scss'
+import cx from 'classnames'
 
 const HeadFilter = () => {
+  const filter = useAppSelector((state) => state.filter)
+  const headlineActive = !!filter.q
+  const datepickerActive = !!filter.begin_date
   const dispatch = useAppDispatch()
   const onClickFilter = () => {
     dispatch(openModal())
@@ -13,19 +17,19 @@ const HeadFilter = () => {
     <div className={styles.container}>
       <ul className={styles.filters}>
         <li>
-          <button type='button' onClick={onClickFilter}>
+          <button type='button' onClick={onClickFilter} className={cx({ [styles.filterActive]: headlineActive })}>
             <MagnifierIcon className={styles.filterIcon} />
-            <span>전체 헤드라인</span>
+            <span>{headlineActive ? `${filter.q}` : '전체 헤드라인'}</span>
           </button>
         </li>
         <li>
-          <button type='button' onClick={onClickFilter}>
+          <button type='button' onClick={onClickFilter} className={cx({ [styles.filterActive]: datepickerActive })}>
             <CalenderIcon className={styles.filterIcon} />
-            <span>전체 날짜</span>
+            <span>{datepickerActive ? `${filter.begin_date}` : '전체 날짜'}</span>
           </button>
         </li>
         <li>
-          <button type='button' onClick={onClickFilter}>
+          <button type='button' onClick={onClickFilter} className={cx({ [styles.filterActive]: datepickerActive })}>
             <span>전체 국가</span>
           </button>
         </li>
